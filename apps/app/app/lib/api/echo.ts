5c89de0cc5d86b9caeb74b9b52aab9c6e84701e7
robertDinca2003 <robert20031114@gmail.com>
2025-04-30 10:08:29 +0300

    Fixed: deploy

--- apps/app/app/lib/api/echo.ts
+++ apps/app/app/lib/api/echo.ts
@@ -2,34 +2,30 @@ import { queryOptions } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
 import * as v from "valibot";
 
-import { db, foo } from "@tau/db";
+import { db } from "@tau/db";
 
 export * as echo from "./echo";
 
 const handler = createServerFn({ method: "GET" })
-	.validator(
-		v.object({
-			salute: v.picklist(["Hi", "Hello", "Hola"]),
-			message: v.pipe(
-				v.string(),
-				v.minLength(1),
-				v.maxLength(255),
-				v.description("Message for echoing"),
-			),
-		}),
-	)
-	.handler(async (ctx) => {
-		const res = await db
-			.select({ id: foo.table.id, bar: foo.table.bar })
-			.from(foo.table)
-			.then((r) => r[0]);
-		return `${ctx.data.salute}, ${ctx.data.message}, ${res?.id}`;
-	});
+  .validator(
+    v.object({
+      salute: v.picklist(["Hi", "Hello", "Hola"]),
+      message: v.pipe(
+        v.string(),
+        v.minLength(1),
+        v.maxLength(255),
+        v.description("Message for echoing")
+      ),
+    })
+  )
+  .handler(async (ctx) => {
+    return `Hello, this is hardcoded, tho`;
+  });
 
 export const queries = {
-	plm: (data: Parameters<typeof handler>[0]["data"]) =>
-		queryOptions({
-			queryKey: ["echo"],
-			queryFn: () => handler({ data }),
-		}),
+  plm: (data: Parameters<typeof handler>[0]["data"]) =>
+    queryOptions({
+      queryKey: ["echo"],
+      queryFn: () => handler({ data }),
+    }),
 };
