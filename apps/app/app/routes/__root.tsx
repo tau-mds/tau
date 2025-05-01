6f1c17ef2b9ed2e08208a82ab43293570fc39b6d
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2025-05-01 15:40:01 +0300

    add sonner and toaster

--- apps/app/app/routes/__root.tsx
+++ apps/app/app/routes/__root.tsx
@@ -1,11 +1,11 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
-	HeadContent,
-	Outlet,
-	Scripts,
-	createRootRouteWithContext,
-	useRouter,
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRouteWithContext,
+  useRouter,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type React from "react";
@@ -18,69 +18,71 @@ import styles from "~/root.css?url";
 import { authClient } from "@tau/auth-client";
 import { useEffect } from "react";
 import { ErrorBoundary } from "~/components/error-boundary";
+import { Toaster } from "@tau/ui";
 
 type Context = {
-	queryClient: QueryClient;
+  queryClient: QueryClient;
 };
 
 export const Route = createRootRouteWithContext<Context>()({
-	head: () => ({
-		meta: [
-			{ charSet: "utf-8" },
-			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ title: "Tau" },
-		],
-		links: [{ rel: "stylesheet", href: styles }],
-	}),
-	loader: async (ctx) => {
-		await ctx.context.queryClient.ensureQueryData(theme.queries.get());
-	},
-	component: Component,
-	errorComponent: (props) => (
-		<Document>
-			<ErrorBoundary {...props} />
-		</Document>
-	),
+  head: () => ({
+    meta: [
+      { charSet: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { title: "Tau" },
+    ],
+    links: [{ rel: "stylesheet", href: styles }],
+  }),
+  loader: async (ctx) => {
+    await ctx.context.queryClient.ensureQueryData(theme.queries.get());
+  },
+  component: Component,
+  errorComponent: (props) => (
+    <Document>
+      <ErrorBoundary {...props} />
+    </Document>
+  ),
 });
 
 function Component() {
-	return (
-		<Document>
-			<Outlet />
-		</Document>
-	);
+  return (
+    <Document>
+      <Outlet />
+      <Toaster richColors position="bottom-center" />
+    </Document>
+  );
 }
 
 function Document(props: Readonly<{ children: React.ReactNode }>) {
-	const themeData = theme.useWithPrefference();
-	const { data } = authClient.useSession();
-	const { navigate } = useRouter();
-	console.log();
+  const themeData = theme.useWithPrefference();
+  const { data } = authClient.useSession();
+  const { navigate } = useRouter();
+  console.log();
 
-	//   TODO: add loading spinner
-	useEffect(() => {
-		if (!data?.user) {
-			if (location.pathname.includes("auth/")) {
-				navigate({ to: "/app" });
-			}
-		}
-	}, [data, navigate]);
+  //   TODO: add loading spinner
+  useEffect(() => {
+    if (!data?.user) {
+      if (location.pathname.includes("auth/")) {
+        navigate({ to: "/app" });
+      }
+    }
+  }, [data, navigate]);
 
-	return (
-		<html lang="en" data-theme={themeData}>
-			<head>
-				<ClientHint />
-				<HeadContent />
-			</head>
+  return (
+    <html lang="en" data-theme={themeData}>
+      <head>
+        <ClientHint />
+        <HeadContent />
+      </head>
 
-			<body>
-				{props.children}
+      <body>
+        {props.children}
 
-				<TanStackRouterDevtools position="bottom-right" />
-				{/* <ReactQueryDevtools buttonPosition="bottom-left" /> */}
-				<ReactQueryDevtools buttonPosition="top-right" />
-				<Scripts />
-			</body>
-		</html>
-	);
+        <TanStackRouterDevtools position="bottom-right" />
+        {/* <ReactQueryDevtools buttonPosition="bottom-left" /> */}
+        <ReactQueryDevtools buttonPosition="top-right" />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
