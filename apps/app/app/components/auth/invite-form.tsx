a5e3fb4d265e1e6aece9a94428ea710483660e5c
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2025-05-02 20:29:57 +0300

    fix linting

--- apps/app/app/components/auth/invite-form.tsx
+++ apps/app/app/components/auth/invite-form.tsx
@@ -1,47 +1,46 @@
 import { Button, Input } from "@tau/ui";
-import { toast } from "sonner";
 import React from "react";
+import { toast } from "sonner";
 // import { authClient } from "@tau/auth-client";
 import { authClient } from "../../../../../packages/auth-client/src/index";
 
 export function InviteForm() {
-  return (
-    <div style={{ margin: "1rem 0" }}>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const form = e.target as HTMLFormElement;
-          const email = (form.elements.namedItem("email") as HTMLInputElement)
-            ?.value;
-          if (email) {
-            await authClient.signIn.magicLink({
-              email,
-              callbackURL: "/app/settings/account/profile",
-            });
-            toast.success("Magic link sent!");
-          }
-        }}
-      >
-        <Input
-          type="email"
-          name="email"
-          placeholder="Enter email"
-          required
-          style={{ marginRight: "0.5rem", padding: "0.5rem" }}
-        />
-        <Button type="submit">Invite</Button>
-      </form>
-      <Button
-        type="button"
-        onClick={() => {
-          //   authClient.forgetPassword({
-          //     email: "tacustefan21@gmail.com",
-          //     redirectTo: "/",
-          //   });
-        }}
-      >
-        Invite
-      </Button>
-    </div>
-  );
+	return (
+		<div style={{ margin: "1rem 0" }}>
+			<form
+				onSubmit={async (e) => {
+					e.preventDefault();
+					const form = e.target as HTMLFormElement;
+					const email = (form.elements.namedItem("email") as HTMLInputElement)?.value;
+					if (email) {
+						await authClient.signIn.magicLink({
+							email,
+							callbackURL: "/app/settings/account/profile",
+						});
+						toast.success("Magic link sent!");
+					}
+				}}
+			>
+				<Input
+					type="email"
+					name="email"
+					placeholder="Enter email"
+					required
+					style={{ marginRight: "0.5rem", padding: "0.5rem" }}
+				/>
+				<Button type="submit">Invite</Button>
+			</form>
+			<Button
+				type="button"
+				onClick={() => {
+					//   authClient.forgetPassword({
+					//     email: "tacustefan21@gmail.com",
+					//     redirectTo: "/",
+					//   });
+				}}
+			>
+				Invite
+			</Button>
+		</div>
+	);
 }
