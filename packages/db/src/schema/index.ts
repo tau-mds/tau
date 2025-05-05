25011eff394d7444736fc9e38d25de9278bae867
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2025-05-05 14:02:11 +0300

    improve db schema and ids

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,22 +1,5 @@
-export * as interview_round from "./interview_round";
-export * as interviewee from "./interviewee";
-export * as interviewer from "./interviewer";
-export * as interview_slot from "./interview_slot";
-export * as authSchema from "./auth-schema";
-
-import { account, organizer, session, verification } from "./auth-schema";
-import { interviewRoundTable } from "./interview_round";
-import { interviewSlotTable } from "./interview_slot";
-import { intervieweeTable } from "./interviewee";
-import { interviewerTable } from "./interviewer";
-
-export const schema = {
-	organizer: organizer,
-	account: account,
-	session: session,
-	verification: verification,
-	interview_round: interviewRoundTable,
-	interviewee: intervieweeTable,
-	interviewer: interviewerTable,
-	interview_slot: interviewSlotTable,
-};
+export * from "./interview_round";
+export * from "./interviewee";
+export * from "./interviewer";
+export * from "./interview_slot";
+export * from "./auth-schema";
