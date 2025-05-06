77252f25581d8ed0de5c17f8f4cb14a7b26eeec9
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2025-05-06 10:14:25 +0300

    fix auth for prod?

--- packages/auth-client/src/auth-client.ts
+++ packages/auth-client/src/auth-client.ts
@@ -9,7 +9,7 @@ import { createAuthClient } from "better-auth/react";
 // export const {useSession} = createAuthClient({
 
 export const authClient = createAuthClient({
-	/** The base URL of the server (optional if you're using the same domain) */
-	baseURL: "http://localhost:3000",
-	plugins: [magicLinkClient()],
+  /** The base URL of the server (optional if you're using the same domain) */
+  // baseURL: "http://localhost:3000",
+  plugins: [magicLinkClient()],
 });
